7ce35104ebad452229b2894a3072246d2bc7b3f8
MPCLabCom <mpclabcom@gmail.com>
2023-11-13 19:28:41 +0900

    Added MecOpenloop

---
+++ 2_OpenLoop_Mec/LEC_CtrlLab_MecOpenLoop/App/AppMotCtrl.h
@@ -0,0 +1,51 @@
+/*
+ * AppMotCtrl.h
+ *
+ *  Created on: 2022. 4. 28.
+ *      Author: MPC_Lab
+ */
+
+#ifndef APPMOTCTRL_H_
+#define APPMOTCTRL_H_
+
+#include "userparms.h"
+#include "userdefines.h"
+
+//===========================================================================
+// Define
+//===========================================================================
+typedef struct {
+    float32     lim;
+    float32     Cnt;
+    float32     CurRef;
+} CUR_TEST;
+
+//===========================================================================
+// Global Variable
+//===========================================================================
+/*===============================Current Control Variable=============================*/
+extern PI_CON      gstCurCtrl;
+
+/*===============================Output Variable=============================*/
+
+/*===============================Speed Control Variable=============================*/
+extern PI_CON       gstSpdCtrl_Tst;
+
+extern float32      gfOmega_vf;
+extern float32      gftheta_vf;
+extern float32      gfVs_vf;
+
+/*======================================================================
+    Variation PWM Frequency
+======================================================================*/
+extern VARI_FREQ    gstVariFreq;
+extern CUR_TEST     gstCurOn;
+//===========================================================================
+// External Function
+//===========================================================================
+void AppMotCtrl_Init(void);
+void AppMotCtrl_Init_PWMOff(void);
+void AppMotCtrl_OpenLoop(void);
+void AppMotCtrl_Cur_H(void);
+
+#endif /* APPMOTCTRL_H_ */
---
+++ 2_OpenLoop_Mec/LEC_CtrlLab_MecOpenLoop/Debug/App/main.d
@@ -0,0 +1,153 @@
+# FIXED
+
+App/main.obj: ../App/main.c
+App/main.obj: ../common_include/F28x_Project.h
+App/main.obj: ../common_include/F2837xD_Cla_typedefs.h
+App/main.obj: ../header_include/F2837xD_device.h
+App/main.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/assert.h
+App/main.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/_ti_config.h
+App/main.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/linkage.h
+App/main.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/stdarg.h
+App/main.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/sys/_types.h
+App/main.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/sys/cdefs.h
+App/main.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/machine/_types.h
+App/main.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/stdbool.h
+App/main.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/stddef.h
+App/main.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/stdint.h
+App/main.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/_stdint40.h
+App/main.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/sys/stdint.h
+App/main.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/machine/_stdint.h
+App/main.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/sys/_stdint.h
+App/main.obj: ../header_include/F2837xD_adc.h
+App/main.obj: ../header_include/F2837xD_analogsubsys.h
+App/main.obj: ../header_include/F2837xD_cla.h
+App/main.obj: ../header_include/F2837xD_cmpss.h
+App/main.obj: ../header_include/F2837xD_cputimer.h
+App/main.obj: ../header_include/F2837xD_dac.h
+App/main.obj: ../header_include/F2837xD_dcsm.h
+App/main.obj: ../header_include/F2837xD_dma.h
+App/main.obj: ../header_include/F2837xD_ecap.h
+App/main.obj: ../header_include/F2837xD_emif.h
+App/main.obj: ../header_include/F2837xD_epwm.h
+App/main.obj: ../header_include/F2837xD_epwm_xbar.h
+App/main.obj: ../header_include/F2837xD_eqep.h
+App/main.obj: ../header_include/F2837xD_flash.h
+App/main.obj: ../header_include/F2837xD_gpio.h
+App/main.obj: ../header_include/F2837xD_i2c.h
+App/main.obj: ../header_include/F2837xD_input_xbar.h
+App/main.obj: ../header_include/F2837xD_ipc.h
+App/main.obj: ../header_include/F2837xD_mcbsp.h
+App/main.obj: ../header_include/F2837xD_memconfig.h
+App/main.obj: ../header_include/F2837xD_nmiintrupt.h
+App/main.obj: ../header_include/F2837xD_output_xbar.h
+App/main.obj: ../header_include/F2837xD_piectrl.h
+App/main.obj: ../header_include/F2837xD_pievect.h
+App/main.obj: ../header_include/F2837xD_sci.h
+App/main.obj: ../header_include/F2837xD_sdfm.h
+App/main.obj: ../header_include/F2837xD_spi.h
+App/main.obj: ../header_include/F2837xD_sysctrl.h
+App/main.obj: ../header_include/F2837xD_upp.h
+App/main.obj: ../header_include/F2837xD_xbar.h
+App/main.obj: ../header_include/F2837xD_xint.h
+App/main.obj: ../common_include/F2837xD_Examples.h
+App/main.obj: ../common_include/F2837xD_GlobalPrototypes.h
+App/main.obj: ../common_include/F2837xD_cputimervars.h
+App/main.obj: ../common_include/F2837xD_Cla_defines.h
+App/main.obj: ../common_include/F2837xD_EPwm_defines.h
+App/main.obj: ../common_include/F2837xD_Adc_defines.h
+App/main.obj: ../common_include/F2837xD_Emif_defines.h
+App/main.obj: ../common_include/F2837xD_Gpio_defines.h
+App/main.obj: ../common_include/F2837xD_I2c_defines.h
+App/main.obj: ../common_include/F2837xD_Ipc_defines.h
+App/main.obj: ../common_include/F2837xD_Pie_defines.h
+App/main.obj: ../common_include/F2837xD_Dma_defines.h
+App/main.obj: ../common_include/F2837xD_SysCtrl_defines.h
+App/main.obj: ../common_include/F2837xD_Upp_defines.h
+App/main.obj: ../common_include/F2837xD_defaultisr.h
+App/main.obj: ../common_include/F2837xD_Ipc_drivers.h
+App/main.obj: ../header_include/userdefines.h
+App/main.obj: ../header_include/userparms.h
+App/main.obj: ../header_include/userinitialize.h
+App/main.obj: ../App/AppADCInput.h
+App/main.obj: ../App/AppFaultCheck.h
+App/main.obj: ../App/AppSeq.h
+App/main.obj: ../App/AppPosSen.h
+App/main.obj: ../App/AppMotCtrl.h
+App/main.obj: ../App/AppDCMtrCtrl.h
+App/main.obj: ../App/AppLab.h
+
+../App/main.c: 
+../common_include/F28x_Project.h: 
+../common_include/F2837xD_Cla_typedefs.h: 
+../header_include/F2837xD_device.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/assert.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/_ti_config.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/linkage.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/stdarg.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/sys/_types.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/sys/cdefs.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/machine/_types.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/stdbool.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/stddef.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/stdint.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/_stdint40.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/sys/stdint.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/machine/_stdint.h: 
+C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/sys/_stdint.h: 
+../header_include/F2837xD_adc.h: 
+../header_include/F2837xD_analogsubsys.h: 
+../header_include/F2837xD_cla.h: 
+../header_include/F2837xD_cmpss.h: 
+../header_include/F2837xD_cputimer.h: 
+../header_include/F2837xD_dac.h: 
+../header_include/F2837xD_dcsm.h: 
+../header_include/F2837xD_dma.h: 
+../header_include/F2837xD_ecap.h: 
+../header_include/F2837xD_emif.h: 
+../header_include/F2837xD_epwm.h: 
+../header_include/F2837xD_epwm_xbar.h: 
+../header_include/F2837xD_eqep.h: 
+../header_include/F2837xD_flash.h: 
+../header_include/F2837xD_gpio.h: 
+../header_include/F2837xD_i2c.h: 
+../header_include/F2837xD_input_xbar.h: 
+../header_include/F2837xD_ipc.h: 
+../header_include/F2837xD_mcbsp.h: 
+../header_include/F2837xD_memconfig.h: 
+../header_include/F2837xD_nmiintrupt.h: 
+../header_include/F2837xD_output_xbar.h: 
+../header_include/F2837xD_piectrl.h: 
+../header_include/F2837xD_pievect.h: 
+../header_include/F2837xD_sci.h: 
+../header_include/F2837xD_sdfm.h: 
+../header_include/F2837xD_spi.h: 
+../header_include/F2837xD_sysctrl.h: 
+../header_include/F2837xD_upp.h: 
+../header_include/F2837xD_xbar.h: 
+../header_include/F2837xD_xint.h: 
+../common_include/F2837xD_Examples.h: 
+../common_include/F2837xD_GlobalPrototypes.h: 
+../common_include/F2837xD_cputimervars.h: 
+../common_include/F2837xD_Cla_defines.h: 
+../common_include/F2837xD_EPwm_defines.h: 
+../common_include/F2837xD_Adc_defines.h: 
+../common_include/F2837xD_Emif_defines.h: 
+../common_include/F2837xD_Gpio_defines.h: 
+../common_include/F2837xD_I2c_defines.h: 
+../common_include/F2837xD_Ipc_defines.h: 
+../common_include/F2837xD_Pie_defines.h: 
+../common_include/F2837xD_Dma_defines.h: 
+../common_include/F2837xD_SysCtrl_defines.h: 
+../common_include/F2837xD_Upp_defines.h: 
+../common_include/F2837xD_defaultisr.h: 
+../common_include/F2837xD_Ipc_drivers.h: 
+../header_include/userdefines.h: 
+../header_include/userparms.h: 
+../header_include/userinitialize.h: 
+../App/AppADCInput.h: 
+../App/AppFaultCheck.h: 
+../App/AppSeq.h: 
+../App/AppPosSen.h: 
+../App/AppMotCtrl.h: 
+../App/AppDCMtrCtrl.h: 
+../App/AppLab.h: 
