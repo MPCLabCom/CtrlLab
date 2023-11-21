39234f3af490667c732921197e1b33dc6722d096
MPCLabCom <mpclabcom@gmail.com>
2023-11-22 00:26:46 +0900

    CurCtrl Added
    Added

---
+++ 3_ClosedLoop_Cur/LEC_CtrlLab_CurCtrl/Debug/common_include/driverlib/subdir_rules.mk
@@ -0,0 +1,15 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Each subdirectory must supply rules for building sources it contributes
+common_include/driverlib/%.obj: ../common_include/driverlib/%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: C2000 Compiler'
+	"C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/bin/cl2000" -v28 -ml -mt --cla_support=cla1 --float_support=fpu32 --tmu_support=tmu0 --vcu_support=vcu2 --include_path="D:/Workspace/For_Lecture/LEC_CtrlLab_CurCtrl" --include_path="../common_include" --include_path="../header_include" --include_path="C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include" --define=CPU1 --define=_FLASH -g --diag_warning=225 --diag_wrap=off --display_error_number --abi=coffabi --preproc_with_compile --preproc_dependency="common_include/driverlib/$(basename $(<F)).d_raw" --obj_directory="common_include/driverlib" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+
